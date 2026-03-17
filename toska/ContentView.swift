ef94f907cb4b15383131f1c17a1f5d1346382fb2
tess <salinarotess@gmail.com>
2026-03-17 09:06:51 -0500

    Initial Commit

---
+++ toska/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toska
+//
+//  Created by Tess Salinaro on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toska/toskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  toskaApp.swift
+//  toska
+//
+//  Created by Tess Salinaro on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct toskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
